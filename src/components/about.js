03a2ba5686fc790e3167a043f699beba80638227
nhng5827 <nhng5827@colorado.edu>
2020-06-23 20:33:46 -0600

    edit about

--- src/components/about.js
+++ src/components/about.js
@@ -13,18 +13,18 @@ render(){
     <div className="about">
       <BrowserView>
       <h1 className="about-header">About the Team</h1><hr/>
-      <p>
+      <p className="about-content">
         Molly Brown was founded in 2010, and is named in tribute to 20th century Colorado suffragette and Titanic survivor Margaret “Molly” Brown (“The Unsinkable Molly Brown”). The team Molly Brown was born from a combination of previous women’s teams in the Boulder/Denver area, including Rare Air and Box. Two of the MB originals, Anna Schott and Anne “Pogo” Pogoriler, developed this name through inspiration from Molly Brown’s philanthropy, volunteerism, and work in improvement of women's lives by continuing education in Colorado. In the spirit of the original Molly Brown's activism, the team has also become known for its activism off the field, both locally and nationally, by supporting women’s and youth participation and development in ultimate.
-<br></br>
+<br></br><br></br>
 Update for the 2020 season: Due to the impact of COVID-19 and uncertainty surrounding the post-season, Molly Brown has opted to forego playing in any tournaments should they occur, and instead will be donating at least $30,000 that would have been spent this season to local BIPOC activists and organizations.
 
       </p>
     </BrowserView>
     <MobileView>
     <h3 className="about-header">About the Team</h3><hr/>
-    <p>
+    <p className="about-content">
       Molly Brown was founded in 2010, and is named in tribute to 20th century Colorado suffragette and Titanic survivor Margaret “Molly” Brown (“The Unsinkable Molly Brown”). The team Molly Brown was born from a combination of previous women’s teams in the Boulder/Denver area, including Rare Air and Box. Two of the MB originals, Anna Schott and Anne “Pogo” Pogoriler, developed this name through inspiration from Molly Brown’s philanthropy, volunteerism, and work in improvement of women's lives by continuing education in Colorado. In the spirit of the original Molly Brown's activism, the team has also become known for its activism off the field, both locally and nationally, by supporting women’s and youth participation and development in ultimate.
-<br></br>
+<br></br><br></br>
 Update for the 2020 season: Due to the impact of COVID-19 and uncertainty surrounding the post-season, Molly Brown has opted to forego playing in any tournaments should they occur, and instead will be donating at least $30,000 that would have been spent this season to local BIPOC activists and organizations.
 
     </p>
